6c7ad2d22e8efcd377905c7ec7a5ef8a47428037
Nitadori Chika <chika@nitadori.jp>
2023-07-27 08:56:17 +0900

    通話通知機能の実装

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const dotenv = require('dotenv');
-const { Client, GatewayIntentBits, Collection, Events, IntentsBitField } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, Events, IntentsBitField, Partials } = require('discord.js');
 
 dotenv.config({ path: './.env' });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent], partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
 
 client.commands = new Collection();
 
@@ -36,6 +36,69 @@ client.on(Events.InteractionCreate, async interaction => {
         await interaction.followUp({ content: 'エラーが起きてしまいました...', ephemeral: true});
     }
 });
+
+//通話通知
+var start_buf=Date.now();
+var end_buf=Date.now();
+
+client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) =>{
+// 開始時の動作
+    if(newGuildMember.channelId != process.env.NOCNSENDID1 && newGuildMember.channelId != process.env.NOCNSENDID2 && newGuildMember.channelId != process.env.NOCNSENDID3 && newGuildMember.channelId != process.env.NOCNSENDID4) {
+        if(oldGuildMember.channelId == null && newGuildMember.channelId != null){
+            if(client.channels.cache.get(newGuildMember.channelId).members.size==1){
+            //時刻等の取得
+            let date = new Date();
+            let nowy = date.getFullYear();
+            let nowm = date.getMonth();
+            let nowd = date.getDate();
+            let nowh = date.getHours();
+            let nowmin = date.getMinutes();
+            let nowsec = date.getSeconds();
+            let nowdate = `${nowy}/${nowm}/${nowd} ${nowh}:${nowmin}:${nowsec}`;
+
+            client.channels.cache.get(`${process.env.TNCHID}`).send({
+                content: `<@&${process.env.TNROLEID}>\n通話が始まったよ〜！覗いてみてね★`,
+                embeds: [{
+                    color: 0xF00035,
+                    timestamp: new Date(),
+                footer: {
+                    text: "ニコふぁんちゃん",
+                },
+                title: `通話開始`,
+                fields: [
+                    {name: `チャンネル`, value: `<#${newGuildMember.channelId}>`, inline: true},
+                    {name: `開始した人`, value: `<@${newGuildMember.id}>`, inline: true},
+                    {name: `開始時間`, value: nowdate, inline: true}
+                ]
+                }],
+            });
+            start_buf = Date.now();
+            }}}
+    //終了時の動作
+    if(newGuildMember.channelId==undefined&&oldGuildMember.channelId!=undefined){
+        if(client.channels.cache.get(oldGuildMember.channelId).members.size==0){
+        const end_buf = Date.now();
+        const totaltime=end_buf-start_buf;
+        const hours=Math.floor(totaltime/1000/60/60)%24;
+        const min=Math.floor(totaltime/1000/60)%60;
+        const sec = Math.floor(totaltime/1000)%60;
+        let times= hours+"時間"+min+"分"+sec+"秒";
+        client.channels.cache.get(`${process.env.TNCHID}`).send({
+            embeds: [{
+                color: 0x001E80,
+                timestamp: new Date(),
+            footer: {
+                text: "ニコふぁんちゃん",
+            },
+            title: `通話終了`,
+            fields: [
+                {name: `チャンネル`, value: `<#${oldGuildMember.channelId}>`, inline: true},
+                {name: `らすとめんばー`, value: `<@${oldGuildMember.id}>`, inline: true},
+                {name: `通話時間`, value: `${times}`, inline: true}
+            ]
+            }],
+        }); 
+}}});
 
 //ログイン
 client.login(process.env.TOKEN); 
